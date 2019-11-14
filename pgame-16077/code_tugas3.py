39115e965e45d7383b174baf553e234ee22ecd2c
RahmawatiLanua <55982304+RahmawatiLanua@users.noreply.github.com>
2019-11-14 20:17:21 +0700

    code_tugas 3

---
+++ pgame-16077/code_tugas3.py
@@ -0,0 +1,9 @@
+# Latihan Buat Variabel : 
+nama = "Nama Saya 	: Rahmawati" 
+print (nama)
+npm = "NPM 		: 121055520116077" 
+print (npm)
+semester = "Semester 	: 7 (Tujuh)" 
+print (semester)
+matakuliah = "Mata Kuliah 	: Pemograman Game" 
+print (matakuliah)
